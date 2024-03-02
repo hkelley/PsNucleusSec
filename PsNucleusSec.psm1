9b06f62432d46c939bddfc3902469add905c6a76
Hugh Kelley <8828244+hkelley@users.noreply.github.com>
2024-03-02 15:50:58 -0500

    minimum viable functions

--- PsNucleusSec.psm1
+++ PsNucleusSec.psm1
@@ -1,6 +1,7 @@
 ﻿
 [datetime] $script:UnixEpoch = '1970-01-01 00:00:00Z'
 
+
 Function Get-Headers ([Parameter(Mandatory = $true)] [string] $ApiKey) {
     return @{'x-apikey' = $ApiKey;
              accept = "application/json"
@@ -9,8 +10,8 @@ Function Get-Headers ([Parameter(Mandatory = $true)] [string] $ApiKey) {
 
 Function Get-NucleusSecProject  (
 	      [Parameter(Mandatory = $true)] [string] $ApiKey
-        , [Parameter(Mandatory = $true)] [string] $ProjectName
         , [Parameter(Mandatory = $true)] [System.Uri] $ApiBaseUrl 
+        , [Parameter(Mandatory = $true)] [string] $ProjectName
     )  {
 
     $url = "{0}/projects" -f $ApiBaseUrl
@@ -28,4 +29,76 @@ Function Get-NucleusSecProject  (
     }
 }
 
+Function Get-NucleusSecFindings (
+	      [Parameter(Mandatory = $true)] [string] $ApiKey
+        , [Parameter(Mandatory = $true)] [System.Uri] $ApiBaseUrl 
+	    , [Parameter(Mandatory = $true)] [int] $ProjectId
+	    , [Parameter(Mandatory = $false)] [int] $AssetId
+        , [Parameter(Mandatory = $false)] [string[]] $Severities
+    ) {
+
+    $ApiLimit = 1000
+    $headers = Get-Headers -ApiKey $ApiKey
+
+    $body = [pscustomobject] @{}
+    if($Asset) { $body | Add-Member -NotePropertyName asset_id -NotePropertyValue $Asset_id }
+    if($Severities) { $body | Add-Member -NotePropertyName finding_severity -NotePropertyValue $Severities }
+
+    if($body.psobject.Properties.Count) {
+        $bodyJson = ConvertTo-Json $body
+    }
+
+    $index = 0 
+    $output = @()
+
+    do {
+        # Use the findings/search endpoint
+        $url = "{0}/projects/{1}/findings/search?start={2}&limit={3}" -f $ApiBaseUrl,$ProjectId,$index,$ApiLimit
+        if($findings = Invoke-RestMethod -Uri $url -Headers $headers -Body $bodyJson -Method Post) {
+            $output += $findings
+        }
+
+    } while ($findings.Count -eq $ApiLimit  -and ($index += $ApiLimit))
+
+    return $output
+}
+
+
+Function Get-NucleusSecAssets (
+	      [Parameter(Mandatory = $true)] [string] $ApiKey
+        , [Parameter(Mandatory = $true)] [System.Uri] $ApiBaseUrl
+	    , [Parameter(Mandatory = $true)] [int] $ProjectId
+        , [Parameter(Mandatory = $false)] [string] $AssetName
+        , [Parameter(Mandatory = $false)] [System.Net.IPAddress] $AssetIP
+    ) {
+
+    $ApiLimit = 5000
+    $headers = Get-Headers -ApiKey $ApiKey
+
+    $index = 0 
+    $output = @()
+
+    do {
+        $url = "{0}/projects/{1}/assets?start={2}&limit={3}" -f $ApiBaseUrl,$ProjectId,$index,$ApiLimit
+
+        if(-not [String]::IsNullOrWhiteSpace($AssetName)) {
+            $url += "&asset_name={0}" -f $AssetName
+        } elseif(-not [String]::IsNullOrWhiteSpace($AssetIP)) {
+            $url += "&ip_address={0}" -f $AssetIP
+        } else {
+            # Get 'em all
+        }
+
+        if($assets = Invoke-RestMethod -Uri $url -Headers $headers -Method Get) {
+            $output += $assets
+        }
+
+    } while ($assets.Count -eq $ApiLimit -and ($index += $ApiLimit))
+
+    return $output
+}
+
+
 Export-ModuleMember -Function Get-NucleusSecProject
+Export-ModuleMember -Function Get-NucleusSecAssets
+Export-ModuleMember -Function Get-NucleusSecFindings
--- README.md
+++ README.md
@@ -3,3 +3,19 @@
 Powershell cmdlets to simplify information collection, team management, and other functions of the https://nucleussec.com platform.
 
 https://help.nucleussec.com/
+
+
+## Quick Start
+
+```
+$par = @{ApiKey='12345';ApiBaseUrl="https://xxxx/nucleus/api"}
+
+$project = Get-NucleusSecProject @par -ProjectName *
+
+$findings = Get-NucleusSecFindings @par -Project $project.project_id -Severities ("Critical","High")
+
+$finding = $findings[0]
+
+Get-NucleusSecAssets @par -ProjectId $project.project_id  -AssetName yyyyy
+
+```
