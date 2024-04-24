e936fab98b6fa39cb5e0ef9d48cae4625078655f
Hugh Kelley <8828244+hkelley@users.noreply.github.com>
2024-04-24 08:38:32 -0400

    added Get-NucleusSecTeamNotableVulns

--- PsNucleusSec.psm1
+++ PsNucleusSec.psm1
@@ -1,12 +1,19 @@
 ﻿
+#region private/helper stuff, no exports here
 [datetime] $script:UnixEpoch = '1970-01-01 00:00:00Z'
-
 
 Function Get-Headers ([Parameter(Mandatory = $true)] [string] $ApiKey) {
     return @{'x-apikey' = $ApiKey;
              accept = "application/json"
             }
 }
+
+Function Get-TotalDaysFromNow ([Parameter(Mandatory = $true)] [datetime] $eventDt) {
+    $ts = New-TimeSpan -Start ([datetime]::UtcNow) -End $eventDt.ToUniversalTime()
+
+    return [int] $ts.TotalDays
+}
+#endregion
 
 Function Get-NucleusSecProject  (
 	      [Parameter(Mandatory = $true)] [string] $ApiKey
@@ -19,9 +26,9 @@ Function Get-NucleusSecProject  (
 
     if($projects = Invoke-RestMethod -Uri $url -Headers $headers) {
         $project = $projects | ?{$_.project_name -like $ProjectName}
-        if(-not $projects) {
+        if(-not $project) {
             throw "Could not find project like '$ProjectName'"
-        } elseif ($projects.Count -gt 1 ) {
+        } elseif ($project.Count -gt 1 ) {
             throw "Multiple projects like '$ProjectName'"
         } else {
             return $project
@@ -34,15 +41,23 @@ Function Get-NucleusSecFindings (
         , [Parameter(Mandatory = $true)] [System.Uri] $ApiBaseUrl 
 	    , [Parameter(Mandatory = $true)] [int] $ProjectId
 	    , [Parameter(Mandatory = $false)] [int] $AssetId
+	    , [Parameter(Mandatory = $false)] [string] $TeamName
         , [Parameter(Mandatory = $false)] [string[]] $Severities
+        , [Parameter(Mandatory = $false)] [int] $ApiLimit = 1000
     ) {
 
-    $ApiLimit = 1000
     $headers = Get-Headers -ApiKey $ApiKey
 
     $body = [pscustomobject] @{}
     if($Asset) { $body | Add-Member -NotePropertyName asset_id -NotePropertyValue $Asset_id }
     if($Severities) { $body | Add-Member -NotePropertyName finding_severity -NotePropertyValue $Severities }
+    if($TeamName) {
+
+        # Verify we were passed a valid team name
+
+
+        $body | Add-Member -NotePropertyName team -NotePropertyValue $TeamName 
+    }
 
     if($body.psobject.Properties.Count) {
         $bodyJson = ConvertTo-Json $body
@@ -77,9 +92,9 @@ Function Get-NucleusSecAssets (
 	    , [Parameter(Mandatory = $true)] [int] $ProjectId
         , [Parameter(Mandatory = $false)] [string] $AssetName
         , [Parameter(Mandatory = $false)] [System.Net.IPAddress] $AssetIP
+        , [Parameter(Mandatory = $false)] [int] $ApiLimit = 5000
     ) {
 
-    $ApiLimit = 5000
     $headers = Get-Headers -ApiKey $ApiKey
 
     $index = 0 
@@ -111,7 +126,79 @@ Function Get-NucleusSecAssets (
     return $output
 }
 
+Function Get-NucleusSecTeam (
+	      [Parameter(Mandatory = $true)] [string] $ApiKey
+        , [Parameter(Mandatory = $true)] [System.Uri] $ApiBaseUrl 
+	    , [Parameter(Mandatory = $true)] [int] $ProjectId
+	    , [Parameter(Mandatory = $false)] [string] $TeamName
+    ) {
+
+    $headers = Get-Headers -ApiKey $ApiKey
+    $url = "{0}/projects/{1}/teams" -f $ApiBaseUrl,$ProjectId
+    $teams = Invoke-RestMethod -Uri $url -Headers $headers
+
+    if($team = ($teams| ?{$_.team_name -eq $TeamName})) {
+        return $team
+    }
+}
+
+Function Get-NucleusSecTeamNotableVulns (
+	      [Parameter(Mandatory = $true)] [string] $ApiKey
+        , [Parameter(Mandatory = $true)] [System.Uri] $ApiBaseUrl 
+	    , [Parameter(Mandatory = $true)] [int] $ProjectId
+	    , [Parameter(Mandatory = $true)] [string] $TeamName
+        , [Parameter(Mandatory = $false)] [int] $TimeWindow = 7
+        , [Parameter(Mandatory = $false)] [string[]] $Severities = ("High", "Critical")
+        , [Parameter(Mandatory = $false)] [int] $ApiLimit = 1000
+    )   {
+
+    $headers = Get-Headers -ApiKey $ApiKey
+
+    # Verify this is a valid team name
+    if(-not ($team = Get-NucleusSecTeam @par -ProjectId $ProjectId -TeamName $TeamName)) {
+        Write-Warning "Could not find team $TeamName in project $ProjectId"
+        return
+    }
+
+    # process the finding severities
+    $notable_findings = @()
+    $notable_findings_keys = @()
+    $findings = Get-NucleusSecFindings -ApiKey $ApiKey -ApiBaseUrl $ApiBaseUrl -ProjectId $ProjectId -Severities $Severities -TeamName $team.team_name
+        
+    foreach($finding in $findings | Sort-Object due_date) {
+
+            # Skip this finding if we've seen a previous one of this name
+            if($notable_findings_keys -contains $finding.finding_name `
+                -or [string]::IsNullOrWhiteSpace($finding.due_date)) {
+                continue
+            }
+
+            $notable_findings_keys += $finding.finding_name
+            $status_message = $null
+
+            if((Get-TotalDaysFromNow -eventDt $finding.due_date) -lt 0 ) {
+                $status_message = "Overdue"
+            } elseif ((Get-TotalDaysFromNow -eventDt $finding.due_date) -lt $TimeWindow ) {
+                $status_message = "Due Soon"
+            } elseif ((Get-TotalDaysFromNow -eventDt $finding.finding_discovered) -ge ($TimeWindow * -1) ) {
+                $status_message = "Recently Discovered"
+            } else {
+                continue    # skip items that aren't new or near/over SLA
+            }
+
+            $finding | Add-Member -NotePropertyName "status_message" -NotePropertyValue $status_message
+            $finding | Add-Member -NotePropertyName "team_name" -NotePropertyValue $finding.justification_assigned_team.team_name
+            $finding.finding_discovered = $finding.finding_discovered.Substring(0,10).Trim()
+
+            $notable_findings += $finding
+    }
+
+    $notable_findings | Select-Object status_message,due_date,finding_discovered,finding_name,finding_severity,team_name
+}
+
 
 Export-ModuleMember -Function Get-NucleusSecProject
 Export-ModuleMember -Function Get-NucleusSecAssets
 Export-ModuleMember -Function Get-NucleusSecFindings
+Export-ModuleMember -Function Get-NucleusSecTeam
+Export-ModuleMember -Function Get-NucleusSecTeamNotableVulns
